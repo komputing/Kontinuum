9f758a8839fffcbb5a7ccffa9976ef072ecf4ead
ligi <ligi@ligi.de>
2017-04-04 19:17:24 +0200

    Deal with checkout errors

--- src/main/kotlin/kontinuum/WorkPackageProcessor.kt
+++ src/main/kotlin/kontinuum/WorkPackageProcessor.kt
@@ -5,6 +5,7 @@ import kontinuum.model.github.GithubCommitState
 import kontinuum.model.github.GithubCommitState.*
 import kontinuum.model.github.GithubCommitStatus
 import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.api.errors.JGitInternalException
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 import java.io.File
 
@@ -20,73 +21,79 @@ fun processWorkPackages() {
 
             val toPath = java.io.File(workspaceDir, currentWorkPackage.project)
 
-
             setStatus(currentWorkPackage, "http://github.com/ligi/kontinuum", pending, "checkout in progress", "checkout")
 
-            val git = if (!toPath.exists()) {
-                Git.cloneRepository()
-                        .setURI("https://x-access-token:" + getToken() + "@github.com/" + currentWorkPackage.project + ".git")
-                        .setCloneSubmodules(true)
-                        .setDirectory(toPath)
-                        .call()
-            } else {
+            val git_res=try {
+                val git = if (!toPath.exists()) {
+                    Git.cloneRepository()
+                            .setURI("https://x-access-token:" + getToken() + "@github.com/" + currentWorkPackage.project + ".git")
+                            .setCloneSubmodules(true)
+                            .setDirectory(toPath)
+                            .call()
+                } else {
 
-                Git(FileRepositoryBuilder().findGitDir(toPath).build()).apply {
-                    fetch().call()
+                    Git(FileRepositoryBuilder().findGitDir(toPath).build()).apply {
+                        fetch().call()
+                    }
                 }
+
+                git.checkout().setName(currentWorkPackage.commitHash).call()
+                git.submoduleUpdate().call()
+                setStatus(currentWorkPackage, "http://github.com/ligi/kontinuum", success, "checkout done", "checkout")
+
+                println("processing commit: " + git.log().setMaxCount(1).call().first().fullMessage)
+                true
+            } catch (e:JGitInternalException) {
+                val errorMessage = "error while checkout: " + e.message
+                setStatus(currentWorkPackage, "http://github.com/ligi/kontinuum", error, errorMessage, "checkout")
+                false
             }
 
-            git.checkout().setName(currentWorkPackage.commitHash).call()
-            git.submoduleUpdate().call()
+            if (git_res) {
+                doIn("spoon", currentWorkPackage, { outPath ->
+                    val spoonResult = executeAndPrint("./gradlew", "clean", "spoon", "-PsingleFlavor", workPath = toPath, outPath = outPath)
+                    println("finished spoon with " + spoonResult)
+                    toPath.walk().filter { it.name == "spoon" }.forEach { it.copyRecursively(File(outPath, it.name), true) }
 
-            println("processing commit: " + git.log().setMaxCount(1).call().first().fullMessage)
+                    if (spoonResult == 0) success else error
+                })
 
-            setStatus(currentWorkPackage, "http://github.com/ligi/kontinuum", success, "checkout done", "checkout")
+                doIn("lint", currentWorkPackage, { outPath ->
 
-            doIn("spoon", currentWorkPackage, { outPath ->
-                val spoonResult = executeAndPrint("./gradlew", "clean", "spoon", "-PsingleFlavor", workPath = toPath, outPath = outPath)
-                println("finished spoon with " + spoonResult)
-                toPath.walk().filter { it.name == "spoon" }.forEach { it.copyRecursively(File(outPath, it.name), true) }
+                    val result = executeAndPrint("./gradlew", "lint", "-PsingleFlavor", workPath = toPath, outPath = outPath)
 
-                if (spoonResult == 0) success else error
-            })
+                    if (result == 0) {
+                        toPath.walk().filter { it.name.startsWith("lint-results") }.forEach { it.copyTo(File(outPath, it.name), true) }
+                        success
+                    } else {
+                        error
+                    }
+                })
 
-            doIn("lint", currentWorkPackage, { outPath ->
+                doIn("test", currentWorkPackage, { outPath ->
 
-                val result = executeAndPrint("./gradlew", "lint","-PsingleFlavor", workPath = toPath, outPath = outPath)
+                    val result = executeAndPrint("./gradlew", "test", "-PsingleFlavor", workPath = toPath, outPath = outPath)
 
-                if (result == 0) {
-                    toPath.walk().filter { it.name.startsWith("lint-results") }.forEach { it.copyTo(File(outPath, it.name), true) }
-                    success
-                } else {
-                    error
-                }
-            })
+                    if (result == 0) {
+                        toPath.walk().filter { it.name == "tests" }.forEach { it.copyTo(File(outPath, it.name), true) }
+                        success
+                    } else {
+                        error
+                    }
+                })
 
-            doIn("test", currentWorkPackage, { outPath ->
+                doIn("assemble", currentWorkPackage, { outPath ->
 
-                val result = executeAndPrint("./gradlew", "test","-PsingleFlavor", workPath = toPath, outPath = outPath)
+                    val result = executeAndPrint("./gradlew", "assembleRelease", workPath = toPath, outPath = outPath)
 
-                if (result == 0) {
-                    toPath.walk().filter { it.name == "tests" }.forEach { it.copyTo(File(outPath, it.name), true) }
-                    success
-                } else {
-                    error
-                }
-            })
-
-            doIn("assemble", currentWorkPackage, { outPath ->
-
-                val result = executeAndPrint("./gradlew", "assembleRelease", workPath = toPath, outPath = outPath)
-
-                if (result == 0) {
-                    toPath.walk().filter { it.name.endsWith(".apk") }.forEach { it.copyTo(File(outPath, it.name), true) }
-                    success
-                } else {
-                    error
-                }
-            })
-
+                    if (result == 0) {
+                        toPath.walk().filter { it.name.endsWith(".apk") }.forEach { it.copyTo(File(outPath, it.name), true) }
+                        success
+                    } else {
+                        error
+                    }
+                })
+            }
         }
         Thread.sleep(1000)
     }
