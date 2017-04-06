6f6b4a763653dd5526e762f67f5d379dd7db790b
ligi <ligi@ligi.de>
2017-04-06 19:35:17 +0200

    Deal with a push that deletes a branch (hence no head commit)

--- src/main/kotlin/kontinuum/WebHookProcessor.kt
+++ src/main/kotlin/kontinuum/WebHookProcessor.kt
@@ -9,7 +9,9 @@ fun processWebHook(event: String, payload: String) {
             val pushInfo = pushEventAdapter.fromJson(payload)
             println("processing push from " + pushInfo.pusher.name + " to " + pushInfo.repository.full_name + " commits:" + pushInfo.commits.size)
 
-            WorkPackageProvider.packages.add(WorkPackage(project = pushInfo.repository.full_name,commitHash = pushInfo.head_commit.id))
+            pushInfo.head_commit?.let {
+                WorkPackageProvider.packages.add(WorkPackage(project = pushInfo.repository.full_name, commitHash = it.id))
+            }
 
         }
     }
--- src/main/kotlin/kontinuum/model/github/GithubPushEvent.kt
+++ src/main/kotlin/kontinuum/model/github/GithubPushEvent.kt
@@ -1,8 +1,11 @@
 package kontinuum.model.github
 
 data class GithubPushEvent(val ref: String,
+                           val created: Boolean,
+                           val deleted: Boolean,
+                           val forced: Boolean,
                            val commits: List<GithubCommit>,
-                           val head_commit: GithubCommit,
+                           val head_commit: GithubCommit?,
                            val compare: String,
                            val pusher: GithubUser,
                            val repository: GithubRepository)
