7632a04e6d128810bee32d75d791391c45bb6044
ligi <ligi@ligi.de>
2017-04-26 11:42:46 +0200

    Cleanup

--- src/main/kotlin/kontinuum/WebServer.kt
+++ src/main/kotlin/kontinuum/WebServer.kt
@@ -1,7 +1,5 @@
 package kontinuum
 
-import kontinuum.model.WorkPackage
-import kontinuum.model.WorkPackageStatus.*
 import org.jetbrains.ktor.application.call
 import org.jetbrains.ktor.http.ContentType
 import org.jetbrains.ktor.netty.embeddedNettyServer
@@ -18,7 +16,7 @@ fun startWebServer() {
             post("/") {
 
                 call.request.headers["X-GitHub-Event"]?.let {
-                    println("processing github event: " + it)
+                    println("processing github event: $it")
                     val payload = call.request.content[String::class]
                     processWebHook(it, payload)
                 }
@@ -27,31 +25,9 @@ fun startWebServer() {
 
             get("/api") {
                 val jsonString = workPackageProviderAdapter.toJson(WorkPackageProvider.packages)
-                call.respondText(jsonString,ContentType.Application.Json)
+                call.respondText(jsonString, ContentType.Application.Json)
             }
 
-            get("/") {
-                var res = "<html><head><title>Kontinuum Progress</title><meta http-equiv=\"refresh\" content=\"2\"></head><body>"
-
-                res += "<h1>Pending WorkPackages</h1>"
-                res += WorkPackageProvider.packages.filter { it.workPackageStatus == PENDING }.toHTML()
-
-                res += "<h1>WorkPackages in progress</h1>"
-                res += WorkPackageProvider.packages.filter { it.workPackageStatus == PROCESSING }.toHTML()
-
-                res += "<h1>Finished WorkPackages</h1>"
-                res += WorkPackageProvider.packages.filter { it.workPackageStatus == FINISHED }.toHTML()
-
-                res += "</body></html>"
-
-                call.respondText(res, ContentType.Text.Html)
-            }
         }
     }.start(wait = false)
-
-
 }
-
-fun List<WorkPackage>.toHTML() = map {
-    it.commitHash + "@" + it.project + "<br/>" + it.stageInfoList.map { it.stage + " " + it.status }.joinToString("<br/>")
-}.joinToString("<hr/>")
