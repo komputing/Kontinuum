c172e312543b6096ce737ca7bf9d8de916861242
ligi <ligi@ligi.de>
2017-05-12 10:19:52 +0200

    Increase read and write timeouts for some spoon reports

--- src/main/kotlin/kontinuum/Env.kt
+++ src/main/kotlin/kontinuum/Env.kt
@@ -10,6 +10,7 @@ import kontinuum.model.config.RepoConfig
 import kontinuum.model.github.*
 import okhttp3.OkHttpClient
 import java.io.File
+import java.util.concurrent.TimeUnit
 
 val configFile = File("kontinuum_config.json")
 
@@ -20,7 +21,9 @@ val outDir = File("out")
 val okhttp = OkHttpClient.Builder().build()!!
 val moshi = Moshi.Builder().build()!!
 
-val ipfs = IPFS()
+val ipfs = IPFS(okHttpClient = OkHttpClient.Builder()
+        .writeTimeout(5, TimeUnit.MINUTES)
+        .readTimeout(5, TimeUnit.MINUTES).build())
 
 val tokenResponseAdapter = moshi.adapter(TokenResponse::class.java)!!
 val commitStatusAdapter = moshi.adapter(GithubCommitStatus::class.java)!!
@@ -30,6 +33,6 @@ val pullRequestEventAdapter = moshi.adapter(GithubPullRequestEvent::class.java)!
 val configAdapter = moshi.adapter(Config::class.java)!!
 val repoConfigAdapter = moshi.adapter(RepoConfig::class.java)!!
 
-val workPackageListType = Types.newParameterizedType(List::class.java,WorkPackage::class.java)
+val workPackageListType = Types.newParameterizedType(List::class.java, WorkPackage::class.java)
 val workPackageProviderAdapter: JsonAdapter<List<WorkPackage>> = moshi.adapter(workPackageListType)!!
 
