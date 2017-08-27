ab862a7fa4cbb7fc9faf556db4e17128186c5614
ligi <ligi@ligi.de>
2017-08-27 13:08:55 +0200

    Continue the processor when one package fails

--- src/main/kotlin/kontinuum/WorkPackageProcessor.kt
+++ src/main/kotlin/kontinuum/WorkPackageProcessor.kt
@@ -23,91 +23,102 @@ val githubInteractor by lazy { GithubApplicationAPI(config.github.integration, F
 fun processWorkPackages() {
 
     while (true) {
+        val packageToProcess = WorkPackageProvider.packages.firstOrNull { it.workPackageStatus == PENDING }
 
-        WorkPackageProvider.packages.firstOrNull { it.workPackageStatus == PENDING }?.let { currentWorkPackage ->
 
-            currentWorkPackage.workPackageStatus = PROCESSING
-
-            println("processing work package: $currentWorkPackage")
-
-            val toPath = java.io.File(workspaceDir, currentWorkPackage.project)
-
-            setStatus(currentWorkPackage, "http://github.com/ligi/kontinuum", pending, "checkout in progress", "checkout")
-
-            val git_res = try {
-                val git = if (!toPath.exists()) {
-                    Git.cloneRepository()
-                            .setURI("https://x-access-token:" + githubInteractor.getToken(currentWorkPackage.installationId) + "@github.com/" + currentWorkPackage.project + ".git")
-                            .setCloneSubmodules(true)
-
-                            .setDirectory(toPath)
-                            .call()
-                } else {
-
-                    Git(FileRepositoryBuilder().findGitDir(toPath).build()).apply {
-                        fetch().call()
-                    }
-                }
-
-                git.checkout().setName(currentWorkPackage.commitHash).call()
-
-                val walk = SubmoduleWalk.forIndex(git.repository)
-                while (walk.next()) {
-                    val submodule = walk.repository
-                    Git.wrap(submodule).fetch().call()
-                    submodule.close()
-                }
-
-                git.submoduleUpdate().call()
-
-                currentWorkPackage.commitMessage = git.log().setMaxCount(1).call().first().fullMessage
-                println("processing commit: " + currentWorkPackage.commitMessage)
-
-                setStatus(currentWorkPackage, "http://github.com/ligi/kontinuum", success, "checkout done", "checkout")
-
-                println("cleaning: ")
-                executeAndPrint("./gradlew clean", toPath)
-                true
-            } catch (e: JGitInternalException) {
-                val errorMessage = "error while checkout: " + e.message
-
-                val hash = ipfs.add.string(e.getStacktraceAsString()).Hash
-                setStatus(currentWorkPackage, hash.hashAsIPFSURL(), error, errorMessage, "checkout")
-                false
+        if (packageToProcess != null)
+            try {
+                processWorkPackage(packageToProcess)
+            } catch (e: Exception) {
+                println("problem in work package $packageToProcess")
+                e.printStackTrace()
             }
-
-            if (git_res) {
-                val configFile = File(toPath, ".ci/kontinuum.json")
-                if (!configFile.exists()) {
-                    println("kontinuum config for repo not found")
-                } else {
-                    val repoConfig = repoConfigAdapter.fromJson(Okio.buffer(Okio.source(configFile)))
-                    var hadError = false
-                    repoConfig?.stages?.forEach {
-                        if (!hadError) {
-                            println("executing stage $it")
-                            val epochSeconds = LocalDateTime.now().atZone(ZoneId.systemDefault()).toEpochSecond()
-                            val stageInfo = StageInfo(it, StageStatus.PENDING, "", epochSeconds)
-                            currentWorkPackage.stageInfoList.add(stageInfo)
-                            executeStageByName(it, currentWorkPackage, toPath, stageInfo)
-                            stageInfo.endEpochSeconds = LocalDateTime.now().atZone(ZoneId.systemDefault()).toEpochSecond()
-                            if (stageInfo.status != StageStatus.SUCCESS) {
-                                hadError = true
-                            }
-                        } else {
-                            println("executing stage $it as another failed")
-                        }
-                    }
-                }
-            }
-
-            currentWorkPackage.workPackageStatus = FINISHED
-        }
 
         Thread.sleep(1000)
     }
 
 }
+
+private fun processWorkPackage(currentWorkPackage: WorkPackage) {
+
+    currentWorkPackage.workPackageStatus = PROCESSING
+
+    println("processing work package: $currentWorkPackage")
+
+    val toPath = File(workspaceDir, currentWorkPackage.project)
+
+    setStatus(currentWorkPackage, "http://github.com/ligi/kontinuum", pending, "checkout in progress", "checkout")
+
+    val git_res = try {
+        val git = if (!toPath.exists()) {
+            Git.cloneRepository()
+                    .setURI("https://x-access-token:" + githubInteractor.getToken(currentWorkPackage.installationId) + "@github.com/" + currentWorkPackage.project + ".git")
+                    .setCloneSubmodules(true)
+
+                    .setDirectory(toPath)
+                    .call()
+        } else {
+
+            Git(FileRepositoryBuilder().findGitDir(toPath).build()).apply {
+                fetch().call()
+            }
+        }
+
+        git.checkout().setName(currentWorkPackage.commitHash).call()
+
+        val walk = SubmoduleWalk.forIndex(git.repository)
+        while (walk.next()) {
+            val submodule = walk.repository
+            Git.wrap(submodule).fetch().call()
+            submodule.close()
+        }
+
+        git.submoduleUpdate().call()
+
+        currentWorkPackage.commitMessage = git.log().setMaxCount(1).call().first().fullMessage
+        println("processing commit: " + currentWorkPackage.commitMessage)
+
+        setStatus(currentWorkPackage, "http://github.com/ligi/kontinuum", success, "checkout done", "checkout")
+
+        println("cleaning: ")
+        executeAndPrint("./gradlew clean", toPath)
+        true
+    } catch (e: JGitInternalException) {
+        val errorMessage = "error while checkout: " + e.message
+
+        val hash = ipfs.add.string(e.getStacktraceAsString()).Hash
+        setStatus(currentWorkPackage, hash.hashAsIPFSURL(), error, errorMessage, "checkout")
+        false
+    }
+
+    if (git_res) {
+        val configFile = File(toPath, ".ci/kontinuum.json")
+        if (!configFile.exists()) {
+            println("kontinuum config for repo not found")
+        } else {
+            val repoConfig = repoConfigAdapter.fromJson(Okio.buffer(Okio.source(configFile)))
+            var hadError = false
+            repoConfig?.stages?.forEach {
+                if (!hadError) {
+                    println("executing stage $it")
+                    val epochSeconds = LocalDateTime.now().atZone(ZoneId.systemDefault()).toEpochSecond()
+                    val stageInfo = StageInfo(it, StageStatus.PENDING, "", epochSeconds)
+                    currentWorkPackage.stageInfoList.add(stageInfo)
+                    executeStageByName(it, currentWorkPackage, toPath, stageInfo)
+                    stageInfo.endEpochSeconds = LocalDateTime.now().atZone(ZoneId.systemDefault()).toEpochSecond()
+                    if (stageInfo.status != StageStatus.SUCCESS) {
+                        hadError = true
+                    }
+                } else {
+                    println("executing stage $it as another failed")
+                }
+            }
+        }
+    }
+
+    currentWorkPackage.workPackageStatus = FINISHED
+}
+
 
 fun doIn(stageInfo: StageInfo, workPackage: WorkPackage, block: (path: File) -> GithubCommitState) {
     println("entering ${stageInfo.stage}")
