50668fc879715bfbd1478f62cc1e6c1e46fcf77d
ligi <ligi@ligi.de>
2019-05-19 21:02:11 +0200

    Error when stage not found

--- common/src/main/kotlin/kontinuum/Stages.kt
+++ common/src/main/kotlin/kontinuum/Stages.kt
@@ -46,11 +46,13 @@ fun executeStageByName(stage: String, currentWorkPackage: WorkPackage, toPath: F
         }
 
         else -> {
-            if (stage.contains("Composer")) {
+            if (stage.contains("Composer"))  {
                 executeGradle(currentWorkPackage, stageInfo, toPath, "$stage -PsingleFlavor") { outPath, _ ->
                     val expectedDir=stage.replace("Composer","").replaceFirst("test","").decapitalize()
                     toPath.walk().filter { it.name == expectedDir }.forEach { it.copyRecursively(File(outPath, it.name), true) }
                 }
+            } else {
+                stageInfo.status = StageStatus.ERROR
             }
         }
     }
