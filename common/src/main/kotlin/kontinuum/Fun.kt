91c4089c0c4e8166bf13ae4114dce3b6cc0dc27c
ligi <ligi@ligi.de>
2019-09-16 19:23:21 +0200

    Use ipfs:// link

--- common/src/main/kotlin/kontinuum/Fun.kt
+++ common/src/main/kotlin/kontinuum/Fun.kt
@@ -3,8 +3,8 @@ package kontinuum
 import java.io.PrintWriter
 import java.io.StringWriter
 
-// at some point in the future we can hopefully use fs:/ipfs
-fun String?.hashAsIPFSURL() = "fs:/ipfs/$this"
+// TODO investigate fs:/ links - as far as I see they should be used instead of ipfs:// - but it did not work with IPFSCompanion
+fun String?.hashAsIPFSURL() = "ipfs://$this"
 
 fun Throwable.getStacktraceAsString(): String {
     val errors = StringWriter()
