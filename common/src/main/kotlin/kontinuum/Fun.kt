0a7d5eb336eede9f19491097a266623b2f9b5be9
ligi <ligi@ligi.de>
2019-09-16 12:25:28 +0200

    Use fs:/ link

--- common/src/main/kotlin/kontinuum/Fun.kt
+++ common/src/main/kotlin/kontinuum/Fun.kt
@@ -4,7 +4,7 @@ import java.io.PrintWriter
 import java.io.StringWriter
 
 // at some point in the future we can hopefully use fs:/ipfs
-fun String?.hashAsIPFSURL() = "https://gateway.ipfs.io/ipfs/$this"
+fun String?.hashAsIPFSURL() = "fs:/ipfs/$this"
 
 fun Throwable.getStacktraceAsString(): String {
     val errors = StringWriter()
